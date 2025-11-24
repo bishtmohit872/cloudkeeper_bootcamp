dc8b6215a848112e05326d65d2e9ff79c1d21786
bishtmohit872 <bishtmohit872@gmail.com>
2025-11-24 17:00:02 +0530

    Adding FaviconIcon in Title Bar

--- CloudBalance/src/dashboard/pages/SideMenuBar.jsx
+++ CloudBalance/src/dashboard/pages/SideMenuBar.jsx
@@ -8,7 +8,7 @@ const SideMenuBar =({state})=>{
                 <div className="h-max py-4">
                     {
                         MenuList.map((menu)=>(
-                            <NavLink key={menu.id} to={menu.path} className={({isActive})=>`${isActive ? " bg-blue-950 rounded-md text-white shadow-lg":""} p-4 w-full flex items-center justify-start text-blue-950 overflow-hidden hover:bg-blue-900 hover:text-white rounded-lg`}>
+                            <NavLink key={menu.id} to={menu.path} className={({isActive})=>`${isActive ? " bg-blue-950 rounded-md text-white shadow-lg":""} mb-4 p-4 w-full flex items-center justify-start text-blue-950 overflow-hidden hover:bg-blue-900 hover:text-white rounded-lg`}>
                                 <span>{menu.logo}</span>
                                 <p className={`ml-2 text-lg text-nowrap transition-opacity duration-300 ${state?'block opacity-100%':'opacity-0'}`}>{menu.name}</p>
                             </NavLink>
